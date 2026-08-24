ad1a0729a81f4a1f3eaeae4ec142bd5018b6491e
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-08-24 07:02:45 +0530

    Create Print the sum of all the subarrays of the given query .cpp

---
+++ Array/Basicproblems/subarray /Print the sum of all the subarrays of the given query .cpp
@@ -0,0 +1,68 @@
+Print the sum of all the subarrays of the given query 
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void querysubarray(int arr[] ,int n)
+{
+  int q;
+  cin >> q;
+  int ans =0;
+  while(q--)
+  {
+    int s, e;
+    cin >> s >> e;
+      int sum =0;
+    for(int i = s; i<=e; i++)
+    { 
+      sum += arr[i];
+    }
+    cout<<sum<<endl;
+  }
+  return ;
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  int arr[n];
+  for(int i=0; i<n; i++)
+  {
+    cin >> arr[i];
+  }
+
+  querysubarray(arr , n);
+}
+
+
+
+
+
+Input:
+10
+10 20 30 40 50 60 89 67 46 100
+query ->10
+s , e->1 3
+a-90
+1 10
+a-554091794
+0 9
+a-512
+7 9 
+a-213
+6 9
+a-302
+1 6
+a-289
+1 8
+a-402
+2 3
+a-70
+5 7
+a-216
+8 9
+a-146
