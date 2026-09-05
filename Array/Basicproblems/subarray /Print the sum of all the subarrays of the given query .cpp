7919a3479fc9af1c3764b533fe7113d1065a4ef9
DIVYALAKSHMI P <47110044+divya-lakshmi-p@users.noreply.github.com>
2026-09-05 12:03:53 +0530

    Update Print the sum of all the subarrays of the given query .cpp

--- Array/Basicproblems/subarray /Print the sum of all the subarrays of the given query .cpp
+++ Array/Basicproblems/subarray /Print the sum of all the subarrays of the given query .cpp
@@ -1,6 +1,50 @@
 Print the sum of all the subarrays of the given query 
 
 
+Prefix sum 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void subarry(int arr[] , int n)
+{
+    vector<int>pref(n);
+
+    for(int i=0; i<n; i++)
+    {
+      (i==0)?pref[i]=arr[i]:pref[i]= pref[i-1]+arr[i];
+    }
+
+      int q;
+      cin >> q;
+      int sum =0;
+    while(q--)
+    {
+      int s ,e;
+      cin >> s>> e;
+      if(s == 0)
+      sum = pref[e];
+      else
+      sum = pref[e]- pref[s-1];
+
+      cout<<sum <<" ";
+    }
+    cout<<endl;
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+
+  int arr[n];
+  for(int i=0; i<n; i++)
+  {
+    cin >> arr[i];
+  }
+
+  subarry(arr , n);
+}
 
 #include<bits/stdc++.h>
 using namespace std;
